a125f90fb29a43c75b968c8d9f0c47575a2cc19a
Hal <haln_01@proton.me>
2025-01-22 15:30:13 -0800

    wip key recovery

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
+import enum
 from re import split
 
 
 CIPHERTEXT = "COTKXNHWJGXABFZPKGJCWGHMYQGEBJYBGQXJIRDCVLVPPWNKSIPXAMTKUQFHQJDKVBGAETTEOGFTSTJVKHKITSLOZYYBANBZALLPIMTKHCMBCTYHPQZNQUPKJFYPPEXDXFGUQMYWEEDXFMOQRECYQVSHDNBMCAXNRZWNTNTJQKEHEXFCKMYSVFHRZVWUSIJOVCOUSKPBKFPRRQKPFNZCRXNTTWKGSEUBMZXURCQEEPPOKWNSHGEPLFKQLATMWMKQBPKSELDVYVPFAWZSMDBMEFNERSXWRGGMBEXDSFDIQRPIGNZTPTZACHHQDTTASREJDLTFLSJLGKJPJKUXIAGNMIAEUFNKVOGVPBXUOHFBIYQLMRZEJOGHQFSYOUUXBWVCBLFLUDGXJCEXEWQZOHZMLAK";
 ALPHABET_LEN = 26
 ENGLISH_IOC = 0.067
+
+def decode(c: str) -> int:
+    return ord(c) - ord('A')
+
+def encode(c: int) -> str:
+    return chr(c + ord('A'))
 
 def decrypt(ciphertext: int, key: int, block_index: int) -> int:
     pt = ciphertext - (key + block_index)
@@ -33,6 +40,33 @@ def calc_stream_ioc(stream: str, key: int = 0) -> float:
     return ioc / (N * (N - 1))
 
 
+def stream_letter_freq(stream: str) -> dict[str, int]:
+    letter_freq = {}
+
+    for block, char in enumerate(stream):
+        decrypted_char = ord(char) - ord('A')
+        decrypted_char = decrypt(decrypted_char, 0, block)
+        decrypted_char += ord('A')
+        decrypted_char = chr(decrypted_char)
+        letter_freq[decrypted_char] = letter_freq.get(decrypted_char, 0) + 1
+
+    return letter_freq
+
+
+def decrypt_ciphertext(ciphertext: str, keys: list[int]) -> str:
+    plaintext = ""
+
+    block_size = len(keys)
+    block_index = -1
+
+    for i, char in enumerate(ciphertext):
+        if i % block_size == 0:
+            block_index += 1
+
+        p = decrypt(decode(char), keys[i % block_size], block_size)
+        plaintext += encode(p)
+
+    return plaintext
 
 if __name__ == "__main__":
     ioc_min = float('inf')
@@ -59,3 +93,12 @@ if __name__ == "__main__":
     keys = [0]*key_len
 
     assert len(streams) == key_len
+
+    for i in range(key_len):
+        letter_freq = stream_letter_freq(streams[i])
+        max_key: str = max(letter_freq, key=letter_freq.get)
+        keys[i] = decode(max_key) % ALPHABET_LEN
+
+    print(f"key {keys} - {"".join([encode(i) for i in keys])}")
+    pt = decrypt_ciphertext(CIPHERTEXT, keys)
+    print(pt)
